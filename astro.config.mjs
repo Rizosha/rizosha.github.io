904eea03e744fabf66a0ee2b1a95776992c2a5d8
Ashley James <ashleytjames2110@gmail.com>
2025-11-27 22:02:48 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rizosha.github.io/RizoshaBlog/",
-	base: "/RizoshaBlog/",
+	site: "https://rizosha.github.io",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
